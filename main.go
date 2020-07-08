42b47ea830c2e204f6689490fadeb63a215b819a
IAMSUTHICHA <suthicha@outlook.com>
2020-07-08 18:18:48 +0700

    add main.go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+var db *sql.DB
+
+var server = "127.0.0.1"
+var port = 1433
+var user = "user"
+var password = "pwd12345"
+var database = "db_demo"
+
+func main() {
+	// Build connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+
+	var err error
+	// Create connection pool
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err.Error())
+	}
+	ctx := context.Background()
+	err = db.PingContext(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Printf("Connected!\n")
+}
